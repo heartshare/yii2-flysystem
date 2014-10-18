58299ccc2a328da17efdf9f51c87882b9978f274
Eugine Terentev <eugine@terentev.net>
2014-10-18 17:08:54 +0300

    first implementation

--- Flysystem.php
+++ Flysystem.php
@@ -11,9 +11,18 @@ use yii\helpers\ArrayHelper;
 
 class Flysystem extends Component
 {
-    public $adapters;
+    /** Array of filesystems configs. Config consists of:
+     *  - adapter: \Closure that returns an object of a class that implements \League\Flysystem\AdapterInterface
+     *  - cache: \Closure that returns an object of a class that implements \League\Flysystem\CacheInterface
+     *  - options: Filesystem options that will be passed to __construct. @see https://github.com/thephpleague/flysystem
+     *  - plugins: array of plugins classes
+     * @var array
+     */
+    public $filesystems;
+    public $filesystemClass = 'League\Flysystem\Filesystem';
 
     private $_loadedSystems = [];
+    private $_mountManager;
 
     public function init()
     {
@@ -22,14 +31,54 @@ class Flysystem extends Component
         }
     }
 
+    /**
+     * @param $name
+     * @return \League\Flysystem\Filesystem
+     * @throws InvalidConfigException
+     */
     public function getFilesystem($name)
     {
         if(!isset($this->_loadedSystems[$name])){
-            $config = ArrayHelper::getValue($this->adapters, $name);
-            if(!$config === null){
+            $config = ArrayHelper::getValue($this->filesystems, $name);
+            if($config === null){
                 throw new InvalidParamException('Unknown filesystem');
             }
-            // to be continued...
+            if(!is_array($config)){
+                throw new InvalidParamException('Invalid filesystem config');
+            }
+            $class = ArrayHelper::getValue($config, 'class', $this->filesystemClass);
+            $adapter = isset($config['adapter']) && is_a($config['adapter'], '\Closure')? call_user_func($config['adapter'], $this) : null;
+            $cache = isset($config['cache']) && is_a($config['cache'], '\Closure')? call_user_func($config['cache'], $this) : null;
+            $options = ArrayHelper::getValue($config, 'options');
+            $plugins = ArrayHelper::getValue($config, 'plugins');
+            $this->_loadedSystems[$name] = \Yii::createObject($class, [
+                $adapter,
+                $cache,
+                $options
+            ]);
+            if($plugins && is_array($plugins)){
+                foreach($plugins as $class){
+                    $this->_loadedSystems[$name]->addPlugin(new $class);
+                }
+            }
+
         }
+        return $this->_loadedSystems[$name];
+    }
+
+    /**
+     * @return null|\League\Flysystem\MountManager
+     * @throws InvalidConfigException
+     */
+    public function getMountManager()
+    {
+        if($this->_mountManager === null){
+            $mountFilesystems = [];
+            foreach(array_keys($this->filesystems) as $name){
+                $mountFilesystems[$name] = $this->getFilesystem($name);
+            }
+            $this->_mountManager = \Yii::createObject('\League\Flysystem\MountManager', $mountFilesystems);
+        }
+        return $this->_mountManager;
     }
 }
